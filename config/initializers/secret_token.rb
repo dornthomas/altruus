38ab57e7e92e0ce18b6735c3cd077014f5dfc1c2
Thomas Dorn <dorn.thomas@gmail.com>
2012-12-11 22:24:35 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Altruus::Application.config.secret_token = '86233cec3073f74eba29761e091239c28bbe4c19a86d05a752e329ee40a0f851f86e02ed931e7b6470daa9d3f4962c4151f6ac36f90d72a76415c473c1b4fbe2'
